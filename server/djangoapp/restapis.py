03736c8019a57dd8de19ab7f786673b21008704f
Mohit Shrivastava <mohitshrivastava@Mohits-MacBook-Air.local>
2022-09-04 12:57:56 +0530

    should work

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,7 +6,7 @@ from requests.auth import HTTPBasicAuth
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
-from django.conf.settings import KEY, URL
+from django.conf import settings
 
 
 # Create a `get_request` to make HTTP GET requests
@@ -97,13 +97,13 @@ def analyze_review_sentiments(text):
 # - Get the returned sentiment label such as Positive or Negative
 
     # Create an authenticator
-    authenticator = IAMAuthenticator(KEY)
+    authenticator = IAMAuthenticator(settings.KEY)
     natural_language_understanding = NaturalLanguageUnderstandingV1(
         version='2020-08-01',
         authenticator=authenticator
     )
 
-    natural_language_understanding.set_service_url(URL)
+    natural_language_understanding.set_service_url(settings.URL)
 
     response = natural_language_understanding.analyze(
         text=text,
