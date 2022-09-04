425f8558428510814c71bf353e1b1c739a8c2df5
Mohit Shrivastava <79797395+icymojitoo@users.noreply.github.com>
2022-09-04 12:09:29 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -97,18 +97,13 @@ def analyze_review_sentiments(text):
 # - Get the returned sentiment label such as Positive or Negative
 
     # Create an authenticator
-
-    creds = {
-        "apikey": os.environ.get('API_KEY'),
-        "url": os.environ.get('API_URL'),
-    }
-    authenticator = IAMAuthenticator(creds['apikey'])
+    authenticator = IAMAuthenticator(os.environ.get('API_KEY'))
     natural_language_understanding = NaturalLanguageUnderstandingV1(
         version='2020-08-01',
         authenticator=authenticator
     )
 
-    natural_language_understanding.set_service_url(creds['url'])
+    natural_language_understanding.set_service_url(os.environ.get('API_URL'))
 
     response = natural_language_understanding.analyze(
         text=text,
